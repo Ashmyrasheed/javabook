52c3e05103d4836204d41cafe40b749e1deaa587
Ashmyrasheed <ashmytr21@gmail.com>
2022-11-06 19:41:15 +0530

    first

--- src/Book.java
+++ src/Book.java
@@ -55,75 +55,18 @@ public class Book {
                     break;
             case 2:
                 System.out.println("select data");
-                try{
-                    Class.forName("com.mysql.jdbc.Driver");
-                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/librarydb","root","");
-                    String sql = "SELECT `id`, `title`, `author`, `category`, `charge per day` FROM `books` ";
-                    Statement stmt = con.createStatement();
-                    ResultSet rs= stmt.executeQuery(sql);
-                    while (rs.next()){
-                        String getbookname=rs.getString("title");
-                        String getauthor=rs.getString("author");
-                        String getcategory=rs.getString("category");
-                        String getbookchargeperday=rs.getString("charge per day");
 
-                        System.out.println("title="+getbookname);
-                        System.out.println("author="+getauthor);
-                        System.out.println("category="+getcategory);
-                        System.out.println("charge per day="+getbookchargeperday);
-
-                    }
-
-                }
-                catch (Exception e){
-                    System.out.println(e);
-                }
                 break;
             case 3:
                 System.out.println("search data");
-                System.out.println("enter bookcharge:");
-                chargeperday=s.nextInt();
-                try{
-                    Class.forName("com.mysql.jdbc.Driver");
-                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/librarydb","root","");
-                    String sql="SELECT `id`, `title`, `author`, `category`, `charge per day` FROM `books` WHERE `charge per day`="+String.valueOf(chargeperday);
-                    Statement stmt = con.createStatement();
-                    ResultSet rs= stmt.executeQuery(sql);
-                    while(rs.next()){
-                        String getbookname=rs.getString("title");
-                        String getauthor=rs.getString("author");
-                        String getcategory=rs.getString("category");
-                        String getbookchargeperday=rs.getString("charge per day");
 
-                        System.out.println("title="+getbookname);
-                        System.out.println("author="+getauthor);
-                        System.out.println("category="+getcategory);
-                        System.out.println("charge per day="+getbookchargeperday);
-
-                    }
-                }
-                catch (Exception e){
-                    System.out.println(e);
-                }
                 break;
             case 4:
                 System.out.println("update data");
                 break;
             case 5:
                 System.out.println("delete data");
-                System.out.println("enter book charge:");
-                String charge=s.next();
-                try{
-                    Class.forName("com.mysql.jdbc.Driver");
-                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/librarydb","root","");
-                    String sql="DELETE FROM `books` WHERE `charge per day`="+charge;
-                    Statement stmt =con.createStatement();
-                    stmt.executeUpdate(sql);
-                    System.out.println("deleted successfully");
-                }
-                catch (Exception e){
-                    System.out.println((e));
-                }
+
                 break;
             case 6:
                 System.out.println("exit");
